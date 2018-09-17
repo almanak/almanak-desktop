aa4c528b0ddd9840b545463f8d8214116baa1d2b
Claus Juhl Knudsen <cjk@aarhus.dk>
2018-09-17 15:24:09 +0200

    .env-var not fetched

--- samdesk/__init__.py
+++ samdesk/__init__.py
@@ -1,20 +1,17 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    # app.config.from_mapping(
-    #     SECRET_KEY='',
-    # )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
         # app.config.from_pyfile('config.py', silent=True)
         # I'm just using direct config for now
         app.config.from_mapping(
-            TESTVAR = 'testvar',
+            CODE_TESTVAR = 'testvar',
         )
     else:
         # load the test config if passed in
@@ -30,7 +27,12 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello')
     def hello():
-        return app.config.get('TESTVAR', 'no testvar')
+        return str(list(app.config.keys()))
+        # return jsonify(app.config)
+
+    @app.route('/env')
+    def env():
+        return app.config.get('TEST_ENV_KEY', 'Missing env-key')
 
     @app.route('/')
     def index():
